e321433ee68ed295df1b9e0a2cbd3b6edcdbe0af
Zhenia <zheniapravdenko@gmail.com>
2025-02-21 14:00:35 +0200

    homework2 - numbers and strings

---
+++ main.js
@@ -0,0 +1,5 @@
+let userName = prompt("What is your name?");
+let userAge = prompt("What is your age?");
+let userEmail = prompt("What is your email address?");
+
+console.log(`Hi, ${userName}! You are ${userAge}, your email address is ${userEmail}`);
