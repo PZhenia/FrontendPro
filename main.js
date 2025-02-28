ef49b183847033d50e5e6cf3601b420f9a55f23e
Zhenia <zheniapravdenko@gmail.com>
2025-02-28 16:55:04 +0200

    homework1 - output numbers to the console

---
+++ main.js
@@ -0,0 +1,25 @@
+//1.1
+console.log("While loop:");
+
+let i = 20;
+
+while(i <= 30){
+    console.log(i);
+    i += 0.5
+}
+//1.2
+console.log("For loop:");
+
+for (let i = 20; i <= 30; i += 0.5) {
+    console.log(i);
+}
+
+//1.3
+console.log("Do while loop:");
+
+let k = 20;
+
+do{
+    console.log(k);
+    k += 0.5;
+} while(k <= 30);
