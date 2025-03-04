0408caf7ef85b7e7151e076b045a0116b304e5d0
Zhenia <zheniapravdenko@gmail.com>
2025-03-04 18:29:32 +0200

    homework2 - arithmetic average

---
+++ main.js
@@ -0,0 +1,30 @@
+function isNum(arr){
+    let numArray = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if (typeof arr[i] === 'number'){
+            numArray.push(arr[i]);
+        }
+    }
+    return numArray;
+}
+
+function arithmeticAverage(num){
+    if (num.length === 0) {
+        return "No numeric elements in the array";
+    } else {
+        let sum = 0;
+
+        for(let i = 0; i < num.length; i++){
+            sum += num[i];
+        }
+        sum = sum / num.length;
+        return sum;
+    }
+}
+
+let arr = ['ahaha', -10, true, 123, 'p', 88];
+let num = isNum(arr);
+console.log(arithmeticAverage(num));
+
+
