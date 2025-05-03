ce4ef30332dbc3f62190b0d2327bdd29f3e49da5
Zhenia <zheniapravdenko@gmail.com>
2025-05-03 13:19:32 +0300

    homework1 - modules

---
+++ js/api.js
@@ -0,0 +1,49 @@
+import {API_URL, GET_POSTS, GET_COMMENTS} from './config.js';
+
+export async function getPosts() {
+    try{
+        const response = await fetch(`${API_URL}${GET_POSTS}`);
+            if(!response.ok){
+                throw new Error(`Failed to get posts - ${response.statusText}`);
+            }
+            return await response.json()
+    } catch (err){
+        console.log(err.message)
+    }
+}
+
+export async function getComments(postId){
+    try{
+        const response = await fetch(`${API_URL}/${postId}${GET_COMMENTS}`);
+            if(!response.ok){
+                throw new Error(`Failed to get comments - ${response.statusText}`);
+            }
+            return await response.json()
+    } catch (err){
+        console.log(err.message)
+    }
+}
+
+export async function addNewPost(title, body){
+    try{
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 1
+            }),
+        });
+
+        if (!response.ok){
+            throw new Error(`Failed to add post - ${response.statusText}`);
+        }
+        return await response.json();
+
+    } catch (err){
+        console.log(err.message)
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import {addNewPost, getPosts} from './api.js';
+import {createPostElement} from './ui.js';
+
+const postsWrapper = document.querySelector('#posts-wrapper');
+const createPostForm = document.querySelector('#create-post-form');
+const titleInput = document.querySelector('#title-input');
+const bodyInput = document.querySelector('#body-input');
+
+async function init(){
+    try{
+        const posts = await getPosts();
+        posts.forEach(post => {
+            const article = createPostElement(post);
+            postsWrapper.appendChild(article);
+        });
+    } catch (err){
+        console.log(err.message);
+    }
+}
+
+createPostForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = titleInput.value.trim();
+    const body = bodyInput.value.trim();
+
+    if(!title || !body){
+        return;
+    }
+
+    addNewPost(title, body).then((post) => {
+        const article    = createPostElement(post);
+        postsWrapper.appendChild(article);
+        titleInput.value = '';
+        bodyInput.value = '';
+    }).catch((err) => {console.log(err.message)});
+});
+init();
